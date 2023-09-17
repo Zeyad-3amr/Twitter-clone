fd1d2abbdb07a6fcdd4042c74ba3c311716803a7
Zeyad-3amr <zeyad.3amr01@gmail.com>
2023-09-18 01:21:16 +0300

    added build file

--- Server/app.js
+++ Server/app.js
@@ -25,10 +25,14 @@ app.use(cookieParser());
 
 app.use('/api/twitter/user', userRouter);
 app.use('/api/twitter/tweet', tweetRouter);
-
+app.use(express.static(`${__dirname}/build`));
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find this url on the server`, 400));
+  res.sendFile(`${__dirname}/build/index.html`);
 });
+
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find this url on the server`, 400));
+// });
 
 app.use(globalErrorHandler);
 module.exports = app;
