38c0462a08d383e92933b7ef30ebd4e305b0512a
Zeyad-3amr <zeyad.3amr01@gmail.com>
2023-09-18 15:47:28 +0300

    updated the project

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api/twitter/',
+  baseURL: 'https://twitter-clone-uxu1.onrender.com/',
   withCredentials: true,
 });
 
